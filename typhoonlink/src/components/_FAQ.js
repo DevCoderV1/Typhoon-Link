a6307f9733e470e6718e1bc182076dff373b4427
M1ck3yZ <m1ck3yz@hotmail.com>
2021-06-16 22:46:43 -0400

    fix faq page

--- typhoonlink/src/components/_FAQ.js
+++ typhoonlink/src/components/_FAQ.js
@@ -16,6 +16,33 @@ const FAQ = () => {
             Mac.
           </p>
         </div>
+        <div className="FAQ">
+          <h2>
+            <strong>2. Why is Typhoon Link created?</strong>
+          </h2>
+          <p>
+            Typhoon Link is a website to show the most popular games for PC and
+            Mac.
+          </p>
+        </div>
+        <div className="FAQ">
+          <h2>
+            <strong>3. Why's the login page not working?</strong>
+          </h2>
+          <p>
+            Typhoon Link is a website to show the most popular games for PC and
+            Mac.
+          </p>
+        </div>
+        <div className="FAQ">
+          <h2>
+            <strong>3. Why's the login page not working?</strong>
+          </h2>
+          <p>
+            Typhoon Link is a website to show the most popular games for PC and
+            Mac.
+          </p>
+        </div>
       </div>
       <Footer />
     </div>
